9e7a6c94697e7ee79fe86a8382eb3922ff732efa
Mike Deverell <mike@pelotoncycle.com>
2017-02-07 13:00:40 -0500

    mapped types don't work with index.ts files

---
+++ src/other/test.ts
@@ -0,0 +1,8 @@
+import {baseNoop} from 'core';
+import {noop} from 'core/noop';
+import {otherNoop} from 'core/other_noop';
+import {yetAnotherNoop} from 'core/other_noop/yet_another_noop';
+
+noop();
+otherNoop();
+yetAnotherNoop();
